69d22a7a32ddac6aa1d572edcd5d80dee85d57e6
unknown <mchmauri@MCH2365N.fsc.net>
2012-01-30 16:34:01 +0100

    10

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 for ($i=1;$i<10;$i++) {
 	echo '<tr>';
-	for ($j=1;$j<6;$j++) {
+	for ($j=1;$j<10;$j++) {
 		echo '<td style="background-color:'.(($i+$j)?'#ff0000':'#00ff00').'">'.$i.'</td>';
 	}
 	echo '</tr>';
