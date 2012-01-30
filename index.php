18ce91b6a5235994bc93adb3de005fceba9fa13f
Maurizio Strasser <maurizio.strasser@ts.fujitsu.com>
2012-01-30 16:48:42 +0100

    This is a buggy test

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php phpinfo();
 
-for ($i=1;$i<10;$i++) {
+for ($i=1;$i<10000;$i++) {
 	echo '<tr>';
-	for ($j=1;$j<10;$j++) {
+	for ($j=1;$j<10000;$j++) {
 		echo '<td style="background-color:'.(($i+$j)?'#ff0000':'#00ff00').'">'.$i.'</td>';
 	}
 	echo '</tr>';
