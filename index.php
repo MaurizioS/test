66d64ca0004b5573ce26138cd83ffc653f1e7484
Maurizio Strasser <maurizio.strasser@ts.fujitsu.com>
2012-01-30 16:42:17 +0100

    10

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php phpinfo();
 
-for ($i=1;$i<100;$i++) {
+for ($i=1;$i<10;$i++) {
 	echo '<tr>';
-	for ($j=1;$j<100;$j++) {
+	for ($j=1;$j<10;$j++) {
 		echo '<td style="background-color:'.(($i+$j)?'#ff0000':'#00ff00').'">'.$i.'</td>';
 	}
 	echo '</tr>';
